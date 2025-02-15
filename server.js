e31b24d32804b2e90277bee2a3eae8bba6504fb9
RaijinD <edecarlosa.16@gmail.com>
2025-02-15 10:32:06 -0300

    some changes to make a static version of the app

--- server.js
+++ server.js
@@ -31,7 +31,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Rota principal
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/view/index.html'));
+    res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 app.listen(8080, () => {
